9472bc3a15f7f4138e275a3f979fb6a5404375d0
jay <iuuh@hotmail.de>
2024-09-09 15:11:03 +0200

    Hidden details

--- Smart_Home_Project/Smart_Home_Project.ino
+++ Smart_Home_Project/Smart_Home_Project.ino
@@ -12,8 +12,8 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 /* WiFi settings */
-const char* ssid = "FRITZ!Box 7530 UR"; // WiFi name
-const char* password = "73905551489688924355"; //WiFi password
+const char* ssid = "yourwifiname"; // WiFi name
+const char* password = "yourpassword"; //WiFi password
 
 // Initialize the server
 WebServer server(80); // Listening on standard port
@@ -28,10 +28,47 @@ void setup() {
   Serial.begin(115200);
   // Starts the DHT sensor
   dht.begin;
+  // Connecting the ESP32 with the WiFi
+  WiFi.begin(ssid, password);
+
+  // Signals the waiting process for the WiFi connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  // Defies a route for the webs server. Entering the ip
+  // of the ESP32 calls the function handleRoot
+  server.on("/", handleRoot);
+  // Starts the web server
+  server.begin();
 
 }
 
 void loop() {
-  // put your main code here, to r:
+  // Checking for new HTTP requests
+  server.handleClient();
+}
+
+// Web server
+void handleRoot() {
+  // Reading the temperature and humidity values
+  float temperature = dht.readTemperature;
+  float humidity = dht.readHumidity;
+
+  // Creating an HTML page to display actual sensor values
+  String html = "<html><head><title>Smart Home Control</title></head><body>";
+  html += "<h1>Monitoring</h1>";
+  html += "<p>Temperature: " + String(temperature) + " °C</p>";
+  html += "<p>Humidity: " + String(humidity) + " %</p>";
+
+  if(temperature > )
+
+  html += "<form method='POST' action='set'>";
+  html += "Threshold temperature: <input type='text' name='temp' value='" + String(tempThreshold) + "'><br>";
+  html += "Threshold humidity: <input type='text' name='hum' value='" + String(humThreshold) + "'><br>";
+  html += "<input type='submit' value='set'>";
+  html += "</form>";
 
 }
